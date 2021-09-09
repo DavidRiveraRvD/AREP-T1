b4f3b9cac3808620761e3c8b6cae844dcbdb0875
DavidRiveraRvD <54150584+DavidRiveraRvD@users.noreply.github.com>
2021-09-09 11:02:14 -0500

    Commit

--- README.md
+++ README.md
@@ -1,1 +1,29 @@
 # AREP-T1
+
+### REQUISITOS
+* Java
+* Maven
+* Git
+* Heroku
+
+### COMPILE
+
+   `mvn package`
+
+### EXECUTE
+
+
+### JAVADOC
+
+Para generar la documentacion java del proyecto ejecute las siguientes instrucciones.
+
+`mvn javadoc:javadoc`
+
+`mvn site`
+
+`mvn site:run`
+
+
+
+### AUTOR
+* David Rivera 
---
+++ src/main/java/edu/eci/arep/app/WebConsumerServiceApp.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package edu.eci.arep.app
+/**
+ *
+ * @author Usuario
+ */
+
+import com.google.gson.Gson;
+import edu.eci.arep.app.ApiConnection.CalculatorAPI;
+import static spark.Spark.*;
+
+
+public class WebConsumerServiceApp {
+
+}
